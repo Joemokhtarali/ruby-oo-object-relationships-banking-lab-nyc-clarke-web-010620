0df1c95814f2fb8d34c2e280b10aa0983125c25f
Mokhtar Ali <joemokhtarali@gmail.com>
2020-01-13 02:30:17 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,8 +18,8 @@ class Transfer
 
   def execute_transaction
     if valid? && sender.balance > amount && self.status == "pending"
-    @sender.balance -= amount
-    @receiver.balance += amount
+      @sender.balance -= amount
+      @receiver.balance += amount
     end 
   end
 
